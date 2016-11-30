9f2006efc75a08bfbb98636a2b355c0c08e9803f
webboy <extramedia.nemanja@gmail.com>
2016-11-30 12:23:19 +0100

    Update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,28 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Post a Job</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Form goes here
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">My Jobs</div>
+                <div class="panel-body">                    
+                    @if (Auth::user()->jobs->count() > 0)
+                        <ul>
+                            @foreach (Auth::user()->jobs as $job)
+                                <li>{{ $job->title }}</li>
+                            @endforeach
+                        </ul>
+                    @else
+                    <p class="text-danger">No jobs posted by You. Use the form above to post your first job</p>
+                    @endif
                 </div>
             </div>
         </div>
