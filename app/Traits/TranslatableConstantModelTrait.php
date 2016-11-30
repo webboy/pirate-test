4dbd0ab7ec0ec402d4d41e7e89ef0b34795ae03d
webboy <extramedia.nemanja@gmail.com>
2016-11-30 11:54:45 +0100

    TranslatableConstantModelTrait

---
+++ app/Traits/TranslatableConstantModelTrait.php
@@ -0,0 +1,23 @@
+<?php  
+namespace Caloboo\Traits;
+
+trait TranslatableConstantModelTrait
+{
+	public static function loadData()
+	{
+		if (empty(self::$data))
+		{
+			$oClass = new \ReflectionClass(__CLASS__);
+
+			$constants = $oClass->getConstants();
+
+			foreach ($constants as $name=>$value)
+			{
+				self::$data[$value]['name'] = trans('constants.'.($oClass->getShortName()).'-'.$name);
+			}
+		}
+
+		return self::$data;
+	}
+}
+?>
